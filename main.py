489a691b2257bac6f9470f578a2d1aa4eb6bf683
Эльнар Менгельбаев <elnar_men@mail.ru>
2022-09-12 13:41:29 +0300

    Change 'main.py'

--- main.py
+++ main.py
@@ -4,59 +4,57 @@ import pip
 import argparse
 from dotenv import load_dotenv
 
-load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
-BASE_URL = 'https://api-ssl.bitly.com/v4/'
-HEADERS = {
-    'Authorization': f'Bearer {TOKEN}',
-    'Content-Type': 'application/json'
-}
+def shorten_link(token, long_link, headers, base_url):
+    url = f'{base_url}bitlinks'
+    payload = {
+        'long_url': long_link
+    }
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    bitlink = response.json()['id']
+    return bitlink
+
+
+def count_clicks(token, bitlink, headers, base_url):
+    url = f'{base_url}bitlinks/{bitlink}/clicks/summary'
+    payload = {
+        'units': '-1',
+        'unit': 'day',
+    }
+    response = requests.get(url, headers=headers, params=payload)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def is_bitlink(link, headers, base_url):
+    url = f'{base_url}bitlinks/{link}'
+
+    response = requests.get(url, headers=headers)
+    if response.ok:
+        return True
+    return False
 
 
 def main():
-    def shorten_link(token, long_link):
-        url = BASE_URL + 'bitlinks'
-        payload = {
-            'long_url': long_link
-        }
-        try:
-            response = requests.post(url, headers=HEADERS, json=payload)
-            response.raise_for_status()
-            link = response.json()['link'].replace('http://', '')\
-                .replace('https://', '')
-            return link
-        except requests.exceptions.HTTPError:
-            return ('Неверный адрес')
-
-    def count_clicks(token, bitlink):
-        url = BASE_URL + f'/bitlinks/{bitlink}/clicks/summary'
-        payload = {
-            'units': '-1',
-            'unit': 'day',
-        }
-        response = requests.get(url, headers=HEADERS, params=payload)
-        response.raise_for_status()
-        return response.json()['total_clicks']
-
-    def is_bitlink(link):
-        url = BASE_URL + f'/bitlinks/{link}'
-
-        response = requests.get(url, headers=HEADERS)
-        if response.ok:
-            return response.json()['link']
-
+    token = os.getenv('BITLY_TOKEN')
+    base_url = 'https://api-ssl.bitly.com/v4/'
+    headers = {'Authorization': f'Bearer {token}'}
     parser = argparse.ArgumentParser(
         description='Программа сокращает длинные ссылки и показывает '
-                    'количество переходов по ссылке'
-    )
-    parser.add_argument('link', help='Введите длинную ссылку для '
-                                     'сокращения или сокращенную для получения статистики')
+                    'количество переходов по ссылке')
+    parser.add_argument('link', help='Введите длинную ссылку'
+                                     ' для сокращения или сокращенную для получения статистики')
     args = parser.parse_args()
-    bitlink = args.link
-    bitlink_info = is_bitlink(bitlink)
-    result = count_clicks(TOKEN, bitlink) if bitlink_info \
-        else shorten_link(TOKEN, bitlink)
+    bitlink_info = is_bitlink(args.link, headers, base_url)
+    if bitlink_info:
+        result = f'По вашей ссылке прошли: {count_clicks(token, args.link, headers, base_url)} раз(а)'
+    else:
+        try:
+            result = f'Битлинк: {shorten_link(token, args.link, headers, base_url)}'
+        except requests.exceptions.HTTPError:
+            result = "Вы ввели неверный адрес"
     print(result)
 if __name__ == "__main__":
+    load_dotenv()
     main()
